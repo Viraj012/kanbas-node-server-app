845f2d90d76002edae23b0038d08051c3adf1d94
Vishal <vishalrajpurohit226@gmail.com>
2024-12-10 20:39:11 -0500

    fixed enrollments

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -112,6 +112,6 @@ export default function UserRoutes(app) {
   app.post("/api/users/signin", signin);
   app.post("/api/users/signout", signout);
   app.post("/api/users/profile", profile);
-  app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
+  // app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
   app.post("/api/users/current/courses", createCourse);
 }
